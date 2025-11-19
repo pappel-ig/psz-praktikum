8d617a41c463a7eaa88475ac89d354c5d8c9e4a6
jonas <jonas@example.org>
2025-11-19 22:07:11 +0100

    naming fix.

--- aufgabe2_fahrstuhl/src/main.rs
+++ aufgabe2_fahrstuhl/src/main.rs
@@ -21,36 +21,34 @@ async fn main() {
     log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info)).unwrap();
 
     // controller -> elevators
-    let (from_controller_to_elevators_tx, _) = broadcast::channel(100);
+    let (controller_to_elevators_tx, _) = broadcast::channel(100);
     // elevator -> controller
     let (elevator_to_controller_tx, elevator_to_controller_rx) = mpsc::channel(100);
     // persons -> controller
     let (person_to_controller_tx, person_to_controller_rx) = mpsc::channel(100);
     // controller -> persons
-    let (from_controller_to_persons_tx, _) = broadcast::channel(100);
+    let (controller_to_persons_tx, _) = broadcast::channel(100);
 
     let controller = ElevatorController::new(
         elevator_to_controller_rx,
-        from_controller_to_elevators_tx.clone(),
+        controller_to_elevators_tx.clone(),
         person_to_controller_rx,
-        from_controller_to_persons_tx.clone(),
+        controller_to_persons_tx.clone(),
         vec!["Dorisch".to_string(), "Ionisch".to_string(), "Korinthisch".to_string()]
     );
     let controller_handle = controller.init();
 
-
-
     let mut threads = vec![
-        Elevator::new("Dorisch", from_controller_to_elevators_tx.subscribe(), elevator_to_controller_tx.clone()).init(),
-        Elevator::new("Ionisch", from_controller_to_elevators_tx.subscribe(), elevator_to_controller_tx.clone()).init(),
-        Elevator::new("Korinthisch", from_controller_to_elevators_tx.subscribe(), elevator_to_controller_tx.clone()).init(),
+        Elevator::new("Dorisch", controller_to_elevators_tx.subscribe(), elevator_to_controller_tx.clone()).init(),
+        Elevator::new("Ionisch", controller_to_elevators_tx.subscribe(), elevator_to_controller_tx.clone()).init(),
+        Elevator::new("Korinthisch", controller_to_elevators_tx.subscribe(), elevator_to_controller_tx.clone()).init(),
         controller_handle,
     ];
 
     delay(500);
 
-    threads.push(Person::with("Alice", from_controller_to_persons_tx.subscribe(), person_to_controller_tx.clone(), First, Ground).init());
-    threads.push(Person::with("Bob", from_controller_to_persons_tx.subscribe(), person_to_controller_tx.clone(), First, Second).init());
+    threads.push(Person::with("Alice", controller_to_persons_tx.subscribe(), person_to_controller_tx.clone(), First, Ground).init());
+    threads.push(Person::with("Bob", controller_to_persons_tx.subscribe(), person_to_controller_tx.clone(), First, Second).init());
 
     delay(500);
 
